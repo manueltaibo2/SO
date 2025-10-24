e8bf0c20a4e3b68a7e0d0e4ae5a222fa97fa5b8e
antonvazquezlopez <anton.vazquez.lopez@udc.es>
2025-10-24 18:16:26 +0200

    aux.h

--- P1/aux.h
+++ P1/aux.h
@@ -1,13 +1,13 @@
 #ifndef aux_h
 #define aux_h
 
-#define _DEFAULT_SOURCE 
+
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
-// Estructura para guardar parámetros de 'dir'
+
 struct DirParams {
     int longFormat;  // 1 = long, 0 = short
     int showLinks;   // 1 = link, 0 = nolink
@@ -15,7 +15,7 @@ struct DirParams {
     int recursion;   // 0 = norec, 1 = reca, 2 = recb
 };
 
-// Variable global accesible desde otros .c
+
 extern struct DirParams dirParams;
 
 // Funciones de ayuda para permisos
